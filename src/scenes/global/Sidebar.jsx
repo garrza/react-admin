307dc75f046c1abd4869c12af0fb6584b5f7c77d
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-09-08 13:59:46 -0600

    Setup link for when we click nav items, takes to to relevant page

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { ProSideBar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return <div>Sidebar</div>
