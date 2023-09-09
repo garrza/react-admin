f579e05963e73efede7179c82628a76222c87111
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-09-09 17:18:14 -0600

    Start team scene using mock data, no api calls

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,5 @@
+import { Box, Typography, useTheme } from "@mui/material"
+import { DataGrid } from "@mui/x-data-grid"
+import { tokens } from "../../theme"
+import { mockDataTeam } from "../../data/mock-data"
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material"
