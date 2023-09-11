9a418f0a3767005654847025c5d9923ab6769c68
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-09-11 16:17:55 -0600

    Add all TextFields for handling user values

--- src/scenes/form/index.js
+++ src/scenes/form/index.js
@@ -1,7 +1,7 @@
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
-import useMediaQuery from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/header";
 
 const initialValues = {
@@ -76,6 +76,81 @@ const Form = () => {
                 helperText={touched.firstName && errors.firstName}
                 sx={{
                   gridColumn: "span 2",
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name="lastName"
+                error={!!touched.lastName && errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{
+                  gridColumn: "span 2",
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && errors.email}
+                helperText={touched.email && errors.email}
+                sx={{
+                  gridColumn: "span 4",
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Contact Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contact}
+                name="contact"
+                error={!!touched.contact && errors.contact}
+                helperText={touched.contact && errors.contact}
+                sx={{
+                  gridColumn: "span 4",
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address 1"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address1}
+                name="address1"
+                error={!!touched.address1 && errors.address1}
+                helperText={touched.address1 && errors.address1}
+                sx={{
+                  gridColumn: "span 4",
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address 2"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address2}
+                name="address2"
+                error={!!touched.address2 && errors.address2}
+                helperText={touched.address2 && errors.address2}
+                sx={{
+                  gridColumn: "span 4",
                 }}
               />
             </Box>
