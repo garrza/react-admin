aee68205e93f8aa76e140f1655ff79296f40821a
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-09-11 16:22:07 -0600

    Complete form submit

--- src/scenes/form/index.js
+++ src/scenes/form/index.js
@@ -154,6 +154,11 @@ const Form = () => {
                 }}
               />
             </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Create New User
+              </Button>
+            </Box>
           </form>
         )}
       </Formik>
