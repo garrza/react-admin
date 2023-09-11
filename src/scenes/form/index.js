5e21310ee3751f3dd76694a0015475ea36c0fc55
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-09-11 16:09:07 -0600

    Validate using yup schema and additional REGEX, use premade Formik params to handle errors and submit

--- src/scenes/form/index.js
+++ src/scenes/form/index.js
@@ -2,3 +2,88 @@ import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material";
+import Header from "../../components/header";
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
+// Additional validation / REGEX for email and phone validation
+const phoneRegEx = /^\+\d{1,4} \d{1,}-\d{1,}-\d{1,}$/;
+
+// Schema to define validation logic for each field we will be using
+const userSchema = yup.object().shape({
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  contact: yup
+    .string()
+    .matches(phoneRegEx, "Invalid phone number")
+    .required("Contact is required"),
+  address1: yup.string().required("Address 1 is required"),
+  address2: yup.string().required("Address 2 is required"),
+});
+
+const Form = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  // Triggering basic boolean if min width is hit
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="CREATE USER" subtitle="Create a New User Profile" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      >
+        {/* Using premade Formik values */}
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              }}
+            >
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="First Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName}
+                name="firstName"
+                error={!!touched.firstName && errors.firstName}
+                helperText={touched.firstName && errors.firstName}
+                sx={{
+                  gridColumn: "span 2",
+                }}
+              />
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+export default Form;
