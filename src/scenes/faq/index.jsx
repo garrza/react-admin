0159cb6fb8b488d4b5d6513eee850ca66ad29d02
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-09-12 15:12:17 -0600

    Complete FAQ page using mui accordion

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -8,7 +8,55 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { tokens } from "../../theme";
 
 const FAQ = () => {
-  
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Quesitons" />
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.purple[500]} variant="h5">
+            First Important Question 
+          </Typography>
+        </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Random text using words and lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis esse suscipit minima maiores libero ratione et nobis aliquid consectetur dicta, veritatis fugit sed inventore nostrum laudantium. Nisi soluta ullam itaque.
+        </Typography>
+      </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.purple[500]} variant="h5">
+            Second Important Question 
+          </Typography>
+        </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Random text using words and lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis esse suscipit minima maiores libero ratione et nobis aliquid consectetur dicta, veritatis fugit sed inventore nostrum laudantium. Nisi soluta ullam itaque.
+        </Typography>
+      </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.purple[500]} variant="h5">
+            Third Important Question 
+          </Typography>
+        </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Random text using words and lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis esse suscipit minima maiores libero ratione et nobis aliquid consectetur dicta, veritatis fugit sed inventore nostrum laudantium. Nisi soluta ullam itaque.
+        </Typography>
+      </AccordionDetails>
+      </Accordion>
+
+
+    </Box>
+  )
 }
 
 export default FAQ;
